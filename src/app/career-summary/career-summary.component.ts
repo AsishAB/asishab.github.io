00a87fda2a3e48ffe76559610ec1e05991e055a6
asishab <asish.bishoi@gmail.com>
2026-06-28 19:40:18 +0530

    Exp calculation

--- src/app/career-summary/career-summary.component.ts
+++ src/app/career-summary/career-summary.component.ts
@@ -1,20 +1,16 @@
 import { Component } from '@angular/core';
-import { 
-  LucideAngularModule, 
-  Mail, 
-  MapPin, 
-  Linkedin, 
-  Github, 
-  Award, 
-  Cloud, 
-  Code2, 
-  Database, 
+import {
+  LucideAngularModule,
+  Award,
+  Cloud,
+  Code2,
   Terminal,
-  Calendar,
   GraduationCap,
   ChevronRight,
   Briefcase
 } from 'lucide-angular';
+
+import { ExperienceService } from '../services/experience.service';
 
 @Component({
   selector: 'app-career-summary',
@@ -23,19 +19,30 @@ import {
   styleUrl: './career-summary.component.css'
 })
 export class CareerSummaryComponent {
-   readonly Award = Award;
+  readonly Award = Award;
   readonly Cloud = Cloud;
   readonly Code2 = Code2;
   readonly Terminal = Terminal;
   readonly GraduationCap = GraduationCap;
   readonly ChevronRight = ChevronRight;
   readonly Briefcase = Briefcase;
-  
-  readonly overallSummary = `
-  Data Engineer with 5+ years of experience in AWS, Spark ETL, Snowflake, Databricks, 
-  and API development, delivering scalable, cost-optimized enterprise solutions. 
-  Primary Skills- AWS , Python , Terraform, Pyspark, Snowflake, Databricks, SQL, NoSSQL (MongoDB)
+
+  readonly totalExperienceYears: string;
+
+  constructor(private readonly experienceService: ExperienceService) {
+    const years = parseFloat(this.experienceService.getTotalExperienceYears());
+    const onlyYear = Math.trunc(years); // → 5
+    this.totalExperienceYears = onlyYear.toString() + "+"; // 5.1 or 5.8 -> 5+
+  }
+
+  get overallSummary(): string {
+    return `
+  Data Engineer with ${this.totalExperienceYears} years of experience in AWS, Spark ETL, Snowflake, Databricks,
+  and API development, delivering scalable, cost-optimized enterprise solutions.
+  \n
+  Primary Skills- AWS , Python , Terraform, Pyspark, Snowflake, Databricks, SQL, NoSQL (MongoDB)
+  \n
   Secondary Skills - NodeJS, Git, CI/CD Tools (Jenkins, Github Actions)
   `;
-
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { 
-  LucideAngularModule, 
-  Mail, 
-  MapPin, 
-  Award, 
-  Cloud, 
-  Code2, 
+import {
+  LucideAngularModule,
+  Mail,
+  MapPin,
+  Award,
+  Cloud,
+  Code2,
   Terminal,
   GraduationCap,
   ChevronRight,
   Briefcase
 } from 'lucide-angular';
+
+import { ExperienceService } from '../services/experience.service';
 
 @Component({
   selector: 'app-header',
@@ -31,38 +33,12 @@ export class HeaderComponent {
   readonly ChevronRight = ChevronRight;
   readonly Briefcase = Briefcase;
 
-  // Calculating total experience based on the experiences array
   totalExperience: string = '0';
-  experiences = [
-    // Infosys
-    {
-      startDate: "2024-07-04"
-    },
-    // TCS
-    {
-      startDate: "2022-07-11",
-      endDate: "2024-05-21",
-    },
-    // Nirmalya Labs
-    {
-      startDate: "2020-06-20",
-      endDate: "2022-06-13",
-    }
-  ]
+
+  constructor(private readonly experienceService: ExperienceService) {}
+
   ngOnInit() {
-    this.calculateTotalExperience();
-  }
-
-  calculateTotalExperience() {
-    const calculateDiff = (start: string, end?: string) => {
-      const startDate = new Date(start);
-      const endDate = end ? new Date(end) : new Date();
-      return endDate.getTime() - startDate.getTime();
-    };
-
-    const totalMs = this.experiences.reduce((acc, exp) => acc + calculateDiff(exp.startDate, exp.endDate), 0);
-    const totalYears = totalMs / (1000 * 60 * 60 * 24 * 365.25);
-    this.totalExperience = totalYears.toFixed(1);
+    this.totalExperience = this.experienceService.getTotalExperienceYears();
   }
 }
 
